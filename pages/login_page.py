11ac33dd2fa2ebfdedf72dfb1bb8e54137cf8aaa
Faithless85 <rainingman@bk.ru>
2020-12-17 21:49:00 +0300

    added transition to login page

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,8 +9,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        login_url = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-        assert login_url == self.browser.current_url, "It is not login url"
+        assert 'login' in self.browser.current_url, "It is not login url"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not presented"
